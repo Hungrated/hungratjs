990d7675a6cb53e08ca95ee75bea9a95ae49e4ad
Zihang Zhang <zhang295415658@qq.com>
2018-02-10 19:12:29 +0800

    doc: uuid.js

--- src/uuid.js
+++ src/uuid.js
@@ -5,6 +5,11 @@
  * @author Hungrated zhang295415658@qq.com
  */
 
+/**
+ * 生成全局唯一标识码
+ * @function generate
+ * @returns {String} 全局唯一标识码
+ */
 const generate = () => {
   return (((1 + Math.random()) * 0x1000000) | 0).toString(16).substring(1);
 };
