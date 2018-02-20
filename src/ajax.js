fd1192f0d53f0e2b0ce89ccb774ba3956631efa6
hungrated <zhang295415658@qq.com>
2018-02-21 03:09:19 +0800

    doc: ajax.js

--- src/ajax.js
+++ src/ajax.js
@@ -1,4 +1,19 @@
+/**
+ * AJAx方法模块
+ * `封装原生ajax方法`
+ * @module Ajax
+ * @see module:Ajax
+ * @author Hungrated zhang295415658@qq.com
+ */
+
 /* eslint-disable no-undef */
+
+/**
+ * 根据数据生成查询字符串
+ * @function createQueryString
+ * @param {Object} _data 数据对象
+ * @returns {String} 查询字符串
+ */
 const createQueryString = (_data) => {
   let queryString = '';
   for (let p in _data) {
@@ -7,6 +22,12 @@ const createQueryString = (_data) => {
   return queryString.substring(1);
 };
 
+/**
+ * 原生AJAX方法
+ * @function ajax
+ * @param {Object} _reqObj 数据对象
+ * @param {Object} _contentType 数据类型
+ */
 const ajax = (_reqObj, _contentType) => {
   let xhr = null;
   window.XMLHttpRequest
@@ -41,6 +62,14 @@ const ajax = (_reqObj, _contentType) => {
   };
 };
 
+/**
+ * AJAX的get方法
+ * @function get
+ * @param {String} _url 目标地址
+ * @param {Object} _data 数据对象
+ * @param {Function} _sucCb 成功回调方法
+ * @returns {String} 查询字符串
+ */
 const get = (_url, _data, _sucCb) => {
   if (typeof _data === 'object') {
     _url = _url + '?' + createQueryString(_data) + `&t=${Math.random().toString().substring(2)}`;
@@ -56,6 +85,14 @@ const get = (_url, _data, _sucCb) => {
   });
 };
 
+/**
+ * AJAX的post方法
+ * @function post
+ * @param {String} _url 目标地址
+ * @param {Object} _data 数据对象
+ * @param {Object} _contentType 数据类型
+ * @param {Function} _sucCb 成功回调方法
+ */
 const post = (_url, _data, _contentType, _sucCb) => {
   ajax({
     method: 'POST',
@@ -66,6 +103,13 @@ const post = (_url, _data, _contentType, _sucCb) => {
   }, _contentType);
 };
 
+/**
+ * 针对JSON数据类型对AJAX的post方法进行封装
+ * @function postJSON
+ * @param {String} _url 目标地址
+ * @param {Object} _data 数据对象
+ * @param {Function} _sucCb 成功回调方法
+ */
 const postJSON = (_url, _data, _sucCb) => {
   post(_url, _data, 'application/json', _sucCb);
 };
