cfdf21a10d6acf264aed6dea0ad5d7eb3b251fd8
hungrated <zhang295415658@qq.com>
2018-02-21 23:12:48 +0800

    feature: modify file.js

--- src/file.js
+++ src/file.js
@@ -1,4 +1,24 @@
-import * as Fs from 'fs';
+import * as Fs from 'Fs';
+
+const makeDir = function (dir) {
+  Fs.mkdir(dir, 0o777, function (err) {
+    if (err) {
+      console.log('dir: `' + dir + '` exists.');
+    } else {
+      console.log(dir + ' created.');
+    }
+  });
+};
+
+const mkdirIfNotExist = function (dir) {
+  Fs.access(dir, function (err) {
+    if (err && err.code === 'ENOENT') {
+      makeDir(dir);
+    } else {
+      console.log('dir: `' + dir + '` exists.');
+    }
+  });
+};
 
 const writeFile = function (path, str) {
   Fs.writeFile(path, str, function (err) {
