7216b07b3b02717715a038ebaf2581272f50a6f4
hungrated <zhang295415658@qq.com>
2018-03-16 16:28:53 +0800

    doc: list.js

--- src/list.js
+++ src/list.js
@@ -6,11 +6,23 @@
  * @author Hungrated zhang295415658@qq.com
  */
 
+/**
+ * 将数组字符串转化为数组对象
+ * @function parseList
+ * @param {String} _str 表示数组的字符串 `元素间用逗号隔开`
+ * @returns {Array} 转化后的数组
+ */
 const parseList = (_str) => {
   return _str.split(',');
 };
 
-// 判断一个元素是否在数组里，不在则更新数组
+/**
+ * 不重复地向数组插入元素
+ * @function parseList
+ * @param {Array} _arr 待处理数组
+ * @param {String|Number|Object} _unit 要插入的项目
+ * @param {String} _cb 回调方法 `第一个参数flag：若元素已存在返回false，否则返回true`
+ */
 const updateIfNotExist = (_arr, _unit, _cb) => {
   if (_arr.indexOf(_unit) === -1) {
     _arr.push(_unit);
@@ -20,22 +32,42 @@ const updateIfNotExist = (_arr, _unit, _cb) => {
   }
 };
 
-// 计算数组和(仅数字数组)
+/**
+ * 计算数组和 `仅数字数组`
+ * @function sum
+ * @param {Array} _arr 待处理数组
+ * @returns {Number} 数组所有元素的和
+ */
 const sum = (_arr) => {
   return _arr.reduce((acc, cur) => (acc + cur), 0);
 };
 
-// 数字数组排序
+/**
+ * 数组排序 `仅数字数组`
+ * @function sortNumbers
+ * @param {Array} _arr 待处理数组
+ * @returns {Array} 排序后数组
+ */
 const sortNumbers = (_arr) => {
   return _arr.sort((a, b) => a - b);
 };
 
-// 数字数组排序(倒序)
+/**
+ * 数组排序（倒序） `仅数字数组`
+ * @function sortNumbersReverse
+ * @param {Array} _arr 待处理数组
+ * @returns {Array} 排序后数组
+ */
 const sortNumbersReverse = (_arr) => {
   return _arr.sort((a, b) => a - b).reverse();
 };
 
-// 数组去重
+/**
+ * 数组去重
+ * @function deduplicate
+ * @param {Array} _arr 待处理数组
+ * @returns {Array} 去重后
+ */
 const deduplicate = (_arr) => {
   return _arr.sort().reduce((_init, _cur) => {
     if (_init.length === 0 || _init[_init.length - 1] !== _cur) {
@@ -45,7 +77,11 @@ const deduplicate = (_arr) => {
   }, []);
 };
 
-// 判断包含
+/**
+ * 判断数组是否包含
+ * @function includes
+ * @returns {Boolean} 判断结果
+ */
 if (!Array.prototype.includes) {
   Object.defineProperty(Array.prototype, 'includes', {
     value: function (searchElement, fromIndex) {
